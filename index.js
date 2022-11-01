42bb6290397609ec00157f614d47726e26aa5c5e
tor <torfrancis447@gmail.com>
2022-11-01 11:51:47 -0400

    commit tree 8c8b702057f09570747c75fc16936e175c7724c5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph") ;
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob") ;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat() {
+   cats.shift(); 
+}
+// const copyOfCats = [...cats];
+function appendCat() {
+    const newCats =[...cats];
+    newCats.push("Broom") ;
+    return newCats
+}
+function prependCat() {
+    const newCats=[...cats];
+    newCats.unshift("Arnold");
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats=[...cats]; 
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats=[...cats];
+    newCats.shift();
+    return newCats;
+}
